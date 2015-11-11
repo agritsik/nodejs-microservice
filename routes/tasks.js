a2836da77af20fa9f0d4489c08c0a8cfa8754b10
andrey <a.gritsik@gmail.com>
2015-11-11 13:25:24 +0200

    added CRUD for Task entity

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,22 +3,45 @@ var Task = require('../models/Task')
 
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-
-
-    Task.create(new Task({name: "hello"}), function (err, result) {
+// create
+router.post('/', function (req, res, next) {
+    Task.create(new Task({name: req.body.name}), function (err, result) {
         if (err) next(err);
-        console.log(result);
+        res.status(201).end();
     });
+});
 
+// get by id
+router.get('/:id', function (req, res, next) {
+    Task.findOne({_id: req.params.id}, function (err, result) {
+        if (err) next(err);
+        res.json(result);
+    })
+});
+
+// get all
+router.get('/', function (req, res, next) {
     Task.find({}, function (err, result) {
         if (err) next(err);
-        console.log(result);
+        res.json(result);
     });
-
-
-    res.send('respond with a resource');
 });
+
+// update
+router.put('/:id', function (req, res, next) {
+    Task.update({_id: req.params.id}, {$set: {name: req.body.name}}, function (err, result) {
+        if (err) next(err);
+        res.status(204).end();
+    });
+});
+
+// delete
+router.delete('/:id', function (req, res, next) {
+    Task.remove({_id: req.params.id}, function (err, result) {
+        if (err) next(err);
+        res.status(204).end();
+    })
+})
+
 
 module.exports = router;
